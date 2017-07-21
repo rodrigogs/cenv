cbe299a36eacc7b21c012464cd5b8d669741c8c5
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2017-07-21 00:55:27 -0300

    Removed unnecessary config file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const debug = require('debug')('cenv:index');
 
 const ConfigFile = require('./ConfigFile');
-const Config = require('./Config');
 const Api = require('./Api');
 
 /**
@@ -9,23 +8,17 @@ const Api = require('./Api');
  * @return {Promise.<void>}
  */
 const cenv = async (options = {}) => {
-  const { environment, file, token, registry, timeout, username, password } = options;
+  const { environment, file } = options;
 
   debug(`loading "${environment}" environment`);
 
-  const configFile = new ConfigFile(file);
-  const config = new Config(configFile);
-  config.registry = registry || config.registry;
-  config.timeout = timeout || config.timeout;
-  config.token = token || config.token;
-  config.username = username || config.username;
-  config.password = password || config.password;
+  const config = new ConfigFile(file);
+  Object.assign(config, options);
 
   const api = new Api(config);
 
-  const variables = await api.environment(environment);
-
-  variables.data.forEach((variable) => {
+  const request = await api.environment(environment);
+  request.data.forEach((variable) => {
     const { name, value } = variable;
     debug(`adding "${name}" variable`);
 
